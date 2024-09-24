137324a11335337d231d37a4dbabd9d8890e8096
Prince John Santillan <mail@princejohnsantillan.net>
2024-09-24 09:45:27 +0800

    Add rector

---
+++ rector.php
@@ -0,0 +1,11 @@
+<?php
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/src',
+    ])
+    ->withPhpSets()
+    ->withPhpPolyfill()
+    ->withPreparedSets(deadCode: true, strictBooleans: true, typeDeclarations: true);
